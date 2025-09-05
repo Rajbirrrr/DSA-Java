b7747ed1b975bcf00d93056f100a24d613bfcc3b
Rajbirrrr <rajbirdey1@gmail.com>
2025-09-06 00:06:21 +0530

    STRINGS

--- STRING/uppercase.java
+++ STRING/uppercase.java
@@ -7,7 +7,7 @@ class uppercase{
         sb.append(ch);
 
         for(int i=1 ; i<str.length(); i++){
-            if(str.charAt(i) == "" && str.charAt(str.length()-1)){
+            if(str.charAt(i) == '\0' && i< (str.length()-1)){
                 sb.append(str.charAt(i));
                 i++;
                 sb.append(Character.toUpperCase(str.charAt(i)));
@@ -15,6 +15,7 @@ class uppercase{
                 sb.append(str.charAt(i));
             }
         }
+        return sb.toString();
    }
    
     public static void main(String args[]){
