eb07fa34d29622c888538a012289c7c4a0d53184
Rajbirrrr <rajbirdey1@gmail.com>
2025-09-08 02:21:12 +0530

    Strings

--- leetcodes/validpalindrome.java
+++ leetcodes/validpalindrome.java
@@ -1,4 +1,6 @@
 class validpalindrome {
+
+    //approach-1:-
     public static boolean isPalindrome(String s) {
         StringBuilder sb = new StringBuilder("");
         StringBuilder sb1 = new StringBuilder("");
@@ -26,9 +28,49 @@ class validpalindrome {
     }
 
 
+     //apprach-2:-
+    public boolean leetcode(String s) {
+        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb1 = new StringBuilder("");
+        
+        //lowercase
+        for(int i=0 ; i<s.length() ; i++){
+            sb.append(Character.toLowerCase(s.charAt(i)));
+        } 
+        //appended only lowercases and digits
+        for(int i=0 ; i<sb.length() ; i++){
+            if(Character.isLetterOrDigit(sb.charAt(i))){
+                sb1.append(sb.charAt(i));
+            }
+        }
+
+        if(sb1.length()==0){
+            return true;
+        }
+
+        if(sb1.length()==1){
+            return true;
+        }
+       
+
+        for(int i=0 ; i<sb1.length()/2 ; i++){
+            if(sb1.charAt(i) != sb1.charAt(sb1.length()-1-i)) {
+                return false;
+            }
+        }
+
+       return true;
+    }
+
+
     public static void main(String args[]){
         String str = "hello I am Raj";
         System.out.println(isPalindrome(str));
+        System.out.println(leetcode(str));
     }
 }
+
+
+
+
 
