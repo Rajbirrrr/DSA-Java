5e8bcdfb8f720c364b6053c017cfa578ff4349fe
Rajbirrrr <rajbirdey1@gmail.com>
2025-09-14 01:15:52 +0530

    TCS

---
+++ BITWISE/noarith.java
@@ -0,0 +1,7 @@
+class noarith{
+    public static void main(String args[]){
+        int n = 10; //1010
+        System.out.println("Muliply by 2:" + " " + ((n<<1)));
+        System.out.println("Divide by 2:" + " " + ((n>>1)));
+    }
+}
